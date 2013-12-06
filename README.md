36c5ef44cf4eea7d136c90dbd3bb6fe872376297
Lily Anachrome <lily.anachrome@gmail.com>
2013-12-05 19:53:35 -0500

    add helper script

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ A markov bot to ~~replace~~ complement avery.
 ## files
 * markov.py -- contains markov code
 * averybot.py -- contains irc code
+* memorize.py -- script turns file into markov pickle
 
 TODO: Make this readme useful.
---
+++ memorize.py
@@ -0,0 +1,19 @@
+# a script to turn a list of lines into a serialized markov dictionary
+import sys
+import pickle
+import markov
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
+        print("usage: " + sys.argv[0] + " <source> <dest> [<order> = 2]")
+    src = sys.argv[1]
+    dst = sys.argv[2]
+    order = 2
+    if len(sys.argv) == 4:
+        order = int(sys.argv[3])
+
+    mark = markov.Markov(order)
+    for line in open(src, 'r'):
+        mark.learn(line)
+
+    pickle.dump(mark, open(dst, 'wb'))
